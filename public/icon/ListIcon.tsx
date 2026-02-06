673d45521f117fa21e09fa82cedf09bb13e29f2d
mahmudul-hasan01 <mahamudul2018@gmail.com>
2026-02-06 15:39:14 +0600

    Remove tabs add link.

--- public/icon/ListIcon.tsx
+++ public/icon/ListIcon.tsx
@@ -2,7 +2,7 @@
 export default function ListIcon() {
     return (
         <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none">
-            <path d="M4.66927 2.91797L11.6693 2.91797M2.33594 2.91797H2.34118M2.33594 7.0013H2.34118M2.33594 11.0846H2.34118M4.66927 7.0013L11.6693 7.0013M4.66927 11.0846L11.6693 11.0846" stroke="#111927" stroke-width="1.33" stroke-linecap="round" />
+            <path d="M4.66927 2.91797L11.6693 2.91797M2.33594 2.91797H2.34118M2.33594 7.0013H2.34118M2.33594 11.0846H2.34118M4.66927 7.0013L11.6693 7.0013M4.66927 11.0846L11.6693 11.0846" stroke="currentColor" stroke-width="1.33" stroke-linecap="round" />
         </svg>
     )
 }
