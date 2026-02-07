07b66f78a83a028beb96bfe1519b37eaf099c6fd
mahmudul-hasan01 <mahamudul2018@gmail.com>
2026-02-07 10:16:58 +0600

    Add Link.

---
+++ app/(admin)/clients/admin/documents/page.tsx
@@ -0,0 +1,8 @@
+import Documents from '@/components/clients/AdminTabs/Documents/Documents'
+import React from 'react'
+
+export default function page() {
+    return (
+        <div><Documents /></div>
+    )
+}
