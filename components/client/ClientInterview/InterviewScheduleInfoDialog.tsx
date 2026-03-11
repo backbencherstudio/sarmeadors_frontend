459c759b4a7a45c22e26ae9d7a613a1ce101e7d5
Parvez Rahman <parvezz13913@gmail.com>
2026-03-11 13:52:41 +0600

    [DONE] Interview schedule info modal design

--- components/client/ClientInterview/InterviewScheduleInfoDialog.tsx
+++ components/client/ClientInterview/InterviewScheduleInfoDialog.tsx
@@ -1,7 +1,7 @@
+import LinkIcon from "@/components/icon/LinkIcon";
 import MeetIcon from "@/components/icon/MeetIcon";
 import TimeRescheduleIcon from "@/components/icon/TimeRescheduleIcon";
 import ButtonReuseable from "@/components/reusable/CustomButton";
-import InformationIcon from "@/public/icon/InformationIcon";
 import { Ellipsis, X } from "lucide-react";
 import { useEffect, useRef, useState } from "react";
 
@@ -67,7 +67,7 @@ function InterviewScheduleInfoDialog({
                   className="flex items-center gap-2 w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
                   onClick={() => alert("Copy Link clicked")}
                 >
-                  <InformationIcon />
+                  <LinkIcon />
                   Copy Link
                 </button>
                 <button
--- components/icon/StripeLogoIcon.tsx
+++ components/icon/StripeLogoIcon.tsx
@@ -20,7 +20,7 @@ export default function StripeLogoIcon() {
             d="M2 0.75H14C14.6904 0.75 15.25 1.30964 15.25 2V14C15.25 14.6904 14.6904 15.25 14 15.25H2C1.30964 15.25 0.75 14.6904 0.75 14V2C0.75 1.30964 1.30964 0.75 2 0.75Z"
             fill="white"
             stroke="white"
-            stroke-width="1.5"
+            strokeWidth="1.5"
           />
         </mask>
         <g mask="url(#mask0_7795_17606)">
