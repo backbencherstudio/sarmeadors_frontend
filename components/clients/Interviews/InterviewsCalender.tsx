d4315ac438c09392834db1654b47c7e67e8afae0
Parvez Rahman <parvezz13913@gmail.com>
2026-03-26 17:18:52 +0600

    [DONE] Interview Request dialog design

--- components/clients/Interviews/InterviewsCalender.tsx
+++ components/clients/Interviews/InterviewsCalender.tsx
@@ -1,40 +1,40 @@
 "use client";
-import CandidateScheduleInfoDialog from "@/components/candidate/CandidateScheduleInfoDialog";
-import CandidateJobRenderSection from "@/components/candidate/myJobs/CandidateJobsRenderSection";
 import FullCalenderCustomize from "@/components/common/FullCalenderCustomize";
-import { jobEvents } from "@/demoData/DashboardData";
+import { interviewEvents } from "@/demoData/DashboardData";
 import { EventContentArg } from "@fullcalendar/core/index.js";
 import { useState } from "react";
+import InterviewRenderSection from "./InterviewRenderSection";
+import InterviewRequestDialog from "./InterviewRequestDialog";
 import InterviewScheduledFilterType from "./InterviewScheduledFilterType";
 
 export default function InterviewsCalender() {
-  const [scheduledData, setScheduledData] = useState<any>(null);
-  const [isOpen, setIsOpen] = useState(false);
+  const [requestInterviewData, setRequestInterviewData] = useState<any>(null);
+  const [isRequestInterviewOpen, setIsRequestInterviewOpen] = useState(false);
 
   const handleOpen = (event: any) => {
     console.log(event, "CHECK");
-    setScheduledData(event);
-    setIsOpen(true);
+    setRequestInterviewData(event);
+    setIsRequestInterviewOpen(true);
   };
 
-  const renderJobEvent = (eventInfo: EventContentArg) => (
-    <CandidateJobRenderSection eventInfo={eventInfo} onOpen={handleOpen} />
+  const renderInterviewEvent = (eventInfo: EventContentArg) => (
+    <InterviewRenderSection eventInfo={eventInfo} onOpen={handleOpen} />
   );
   return (
     <div className="w-full rounded-xl bg-white ">
       <div>
         <FullCalenderCustomize
           filterSection={<InterviewScheduledFilterType />}
-          renderEvent={renderJobEvent}
-          data={jobEvents}
+          renderEvent={renderInterviewEvent}
+          data={interviewEvents}
         />
       </div>
 
-      {isOpen && scheduledData && (
-        <CandidateScheduleInfoDialog
-          isOpen={isOpen}
-          setOpen={() => setIsOpen(false)}
-          data={scheduledData}
+      {isRequestInterviewOpen && requestInterviewData && (
+        <InterviewRequestDialog
+          isOpen={isRequestInterviewOpen}
+          setOpen={() => setIsRequestInterviewOpen(false)}
+          data={requestInterviewData}
         />
       )}
     </div>
