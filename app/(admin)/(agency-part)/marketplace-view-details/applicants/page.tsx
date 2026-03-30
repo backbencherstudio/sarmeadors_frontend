7eadd677989df78b64d6f6715d5c17780f123674
Parvez Rahman <parvezz13913@gmail.com>
2026-03-30 11:04:12 +0600

    [DONE] Applicants header and stat card design

--- app/(admin)/(agency-part)/marketplace-view-details/applicants/page.tsx
+++ app/(admin)/(agency-part)/marketplace-view-details/applicants/page.tsx
@@ -1,9 +1,56 @@
 import ApplicantTopBar from "@/components/clients/AgencyShortTermJob/ApplicantTopBar";
+import StatCards from "@/components/dashboard/StatCards";
+
+const statCards = [
+  {
+    title: "Pre Application",
+    value: 195,
+    percentage: "0.1%",
+  },
+  {
+    title: "Application Started",
+    value: 7,
+    percentage: "0.8%",
+  },
+  {
+    title: "Applied",
+    value: 18,
+    percentage: "1.5%",
+  },
+  {
+    title: "Inactive",
+    value: 635,
+    percentage: "72.6%",
+  },
+  {
+    title: "Initial Payment Made",
+    value: 0,
+    percentage: "3.2%",
+  },
+  {
+    title: "Consultation Booked",
+    value: 0,
+    percentage: "3.2%",
+  },
+  {
+    title: "Consultation Complete",
+    value: 0,
+    percentage: "3.2%",
+  },
+  {
+    title: "Job Posted",
+    value: 97,
+    percentage: "3.2%",
+  },
+];
 
 export default function page() {
   return (
     <div className="p-6">
       <ApplicantTopBar />
+      <div className="pt-12">
+        <StatCards statCards={statCards} />
+      </div>
     </div>
   );
 }
