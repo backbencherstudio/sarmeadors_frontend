f18b5fced5af9b7b39781c7e6a4ea1575c28e407
Parvez Rahman <parvezz13913@gmail.com>
2026-04-10 09:00:07 +0600

    [DONE] Candidate review card design

--- components/clients/AgencyShortTermJob/CandidateReviewCard.tsx
+++ components/clients/AgencyShortTermJob/CandidateReviewCard.tsx
@@ -1,3 +1,4 @@
+import StarIcon from "@/components/icon/StarIcon";
 import Image from "next/image";
 
 export default function CandidateReviewCard({ data }) {
@@ -15,6 +16,16 @@ export default function CandidateReviewCard({ data }) {
           <div>
             <h1 className="text-[#111927] font-medium">{data?.name}</h1>
             <p className="text-[#B9BDC2] ">{data?.role}</p>
+          </div>
+        </div>
+        <div>
+          <p className="text-[#111927] mt-4">{data?.description}</p>
+          <div className="mt-8 flex items-center justify-between">
+            <div className="flex items-center gap-2">
+              <StarIcon className="text-[#E5B400]" />
+              <p className="text-[#111927]">(5.0)</p>
+            </div>
+            <p className="text-[#111927]">{data?.date}</p>
           </div>
         </div>
       </div>
