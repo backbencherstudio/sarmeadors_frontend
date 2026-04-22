8fe0da231beb648d0d2a3b3c5c25b1dea087763e
Parvez Rahman <parvezz13913@gmail.com>
2026-04-22 16:58:38 +0600

    [DONE] Message page design

--- components/common/Sidebar.tsx
+++ components/common/Sidebar.tsx
@@ -16,7 +16,6 @@ import { usePathname, useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import { RiArrowLeftDoubleFill, RiArrowRightDoubleFill } from "react-icons/ri";
 import AppliedJobIcon from "../icon/ApplideJobIcon";
-import CalenderIcon from "../icon/CalenderIcon";
 import CandidateIcon from "../icon/CandidateIcon";
 import ClientIcon from "../icon/ClientIcon";
 import DashboardIcon from "../icon/DashboardIcon";
@@ -26,7 +25,6 @@ import LogoutIcon from "../icon/LogoutIcon";
 import MultiPleUserIcon from "../icon/MultiPleUserIcon";
 import MyAvailabilityIcon from "../icon/MyAvailabilityIcon";
 import PaymentIcon from "../icon/PaymentIcon";
-import PlatFormIcon from "../icon/PlatFormIcon";
 import SettingIcon from "../icon/SettingIcon";
 import SupportIcon from "../icon/SupportIcon";
 
@@ -75,24 +73,24 @@ const navItems: NavItem[] = [
     href: "/agency-long-term-job/agency-requested-job",
     type: "admin",
   },
-  {
-    icon: CalenderIcon,
-    label: "Shift Job calendar",
-    href: "/dashboard/shift-job-calendar",
-    type: "admin",
-  },
-  {
-    icon: JobsIcon,
-    label: "Placement Jobs",
-    href: "/dashboard/placement-jobs",
-    type: "admin",
-  },
-  {
-    icon: PlatFormIcon,
-    label: "Platform settings",
-    href: "/dashboard/platform-settings",
-    type: "admin",
-  },
+  // {
+  //   icon: CalenderIcon,
+  //   label: "Shift Job calendar",
+  //   href: "/dashboard/shift-job-calendar",
+  //   type: "admin",
+  // },
+  // {
+  //   icon: JobsIcon,
+  //   label: "Placement Jobs",
+  //   href: "/dashboard/placement-jobs",
+  //   type: "admin",
+  // },
+  // {
+  //   icon: PlatFormIcon,
+  //   label: "Platform settings",
+  //   href: "/dashboard/platform-settings",
+  //   type: "admin",
+  // },
   {
     icon: MoreIcon,
     label: "More",
@@ -276,16 +274,18 @@ const Sidebar: React.FC<SidebarProps> = ({
         <div className="flex items-center justify-between mb-6">
           <Link
             href={"/"}
-            className={`text-white flex items-center transition-all duration-300 ${isCollapsed ? "xl:justify-center hidden xl:w-full" : ""
-              }`}
+            className={`text-white flex items-center transition-all duration-300 ${
+              isCollapsed ? "xl:justify-center hidden xl:w-full" : ""
+            }`}
           >
             <Image
               src={mainLogo}
               alt="main logo"
               width={118}
               height={29}
-              className={`transition-all duration-300 ${isCollapsed ? "xl:w-8 xl:h-8 " : "w-20 md:w-[100px]"
-                }`}
+              className={`transition-all duration-300 ${
+                isCollapsed ? "xl:w-8 xl:h-8 " : "w-20 md:w-[100px]"
+              }`}
             />
           </Link>
 
@@ -339,13 +339,15 @@ const Sidebar: React.FC<SidebarProps> = ({
                       <div className="flex gap-2 items-center">
                         <div className="w-[30px] h-[30px] group  flex justify-center items-center flex-shrink-0 text-xl font-medium text-blackColor">
                           <item.icon
-                            className={`opacity-70 group-hover:opacity-100 transition-opacity duration-200 ${active ? "opacity-100" : ""
-                              }`}
+                            className={`opacity-70 group-hover:opacity-100 transition-opacity duration-200 ${
+                              active ? "opacity-100" : ""
+                            }`}
                           />
                         </div>
                         <span
-                          className={`text-base font-medium text-descriptionColor group-hover:text-blackColor transition-colors duration-200 whitespace-nowrap ${isCollapsed ? "xl:hidden" : ""
-                            }`}
+                          className={`text-base font-medium text-descriptionColor group-hover:text-blackColor transition-colors duration-200 whitespace-nowrap ${
+                            isCollapsed ? "xl:hidden" : ""
+                          }`}
                         >
                           {item.label}
                         </span>
@@ -366,9 +368,10 @@ const Sidebar: React.FC<SidebarProps> = ({
                         className={`
                           w-full flex items-center group gap-3 px-3 py-2.5 lg:py-3 rounded-lg 
                           hover:text-whiteColor hover:bg-whiteColor text-blackColor transition-all duration-200
-                          ${isCollapsed
-                            ? "xl:justify-center"
-                            : "justify-between"
+                          ${
+                            isCollapsed
+                              ? "xl:justify-center"
+                              : "justify-between"
                           }
                         `}
                         title={item.label}
@@ -377,20 +380,23 @@ const Sidebar: React.FC<SidebarProps> = ({
                         <div className="flex gap-2 items-center">
                           <div className="w-[30px] h-[30px] group  flex justify-center items-center flex-shrink-0 text-xl font-medium text-blackColor">
                             <item.icon
-                              className={`opacity-70 group-hover:opacity-100 transition-opacity duration-200 ${active ? "opacity-100" : ""
-                                }`}
+                              className={`opacity-70 group-hover:opacity-100 transition-opacity duration-200 ${
+                                active ? "opacity-100" : ""
+                              }`}
                             />
                           </div>
                           <span
-                            className={`text-base font-medium text-descriptionColor group-hover:text-blackColor transition-colors duration-200 whitespace-nowrap ${isCollapsed ? "xl:hidden" : ""
-                              }`}
+                            className={`text-base font-medium text-descriptionColor group-hover:text-blackColor transition-colors duration-200 whitespace-nowrap ${
+                              isCollapsed ? "xl:hidden" : ""
+                            }`}
                           >
                             {item.label}
                           </span>
                         </div>
                         <ChevronRight
-                          className={`transition-transform duration-200 ${moreOpen ? "rotate-90" : ""
-                            }`}
+                          className={`transition-transform duration-200 ${
+                            moreOpen ? "rotate-90" : ""
+                          }`}
                           size={18}
                         />
                       </button>
@@ -407,13 +413,15 @@ const Sidebar: React.FC<SidebarProps> = ({
                               className={`
                                 ml-10 flex items-center group gap-3 px-3 py-1.5 lg:py-2 rounded-lg 
                                 hover:text-whiteColor hover:bg-white text-blackColor transition-all duration-200
-                                ${subActive
-                                  ? "bg-white opacity-100 text-blackColor"
-                                  : ""
+                                ${
+                                  subActive
+                                    ? "bg-white opacity-100 text-blackColor"
+                                    : ""
                                 }
-                                ${isCollapsed
-                                  ? "xl:justify-center"
-                                  : "justify-start"
+                                ${
+                                  isCollapsed
+                                    ? "xl:justify-center"
+                                    : "justify-start"
                                 }
                               `}
                               title={isCollapsed ? sub.label : ""}
@@ -427,8 +435,9 @@ const Sidebar: React.FC<SidebarProps> = ({
                                   /> */}
                                 </div>
                                 <span
-                                  className={`text-base font-medium text-descriptionColor group-hover:text-blackColor transition-colors duration-200 whitespace-nowrap ${isCollapsed ? "xl:hidden" : ""
-                                    }`}
+                                  className={`text-base font-medium text-descriptionColor group-hover:text-blackColor transition-colors duration-200 whitespace-nowrap ${
+                                    isCollapsed ? "xl:hidden" : ""
+                                  }`}
                                 >
                                   {sub.label}
                                 </span>
@@ -457,13 +466,15 @@ const Sidebar: React.FC<SidebarProps> = ({
                   <div className="flex gap-2 items-center">
                     <div className="w-[30px] h-[30px] group  flex justify-center items-center flex-shrink-0 text-xl font-medium text-blackColor">
                       <item.icon
-                        className={`opacity-70 group-hover:opacity-100 transition-opacity duration-200 ${active ? "opacity-100" : ""
-                          }`}
+                        className={`opacity-70 group-hover:opacity-100 transition-opacity duration-200 ${
+                          active ? "opacity-100" : ""
+                        }`}
                       />
                     </div>
                     <span
-                      className={`text-base font-medium text-descriptionColor group-hover:text-blackColor transition-colors duration-200 whitespace-nowrap ${isCollapsed ? "xl:hidden" : ""
-                        }`}
+                      className={`text-base font-medium text-descriptionColor group-hover:text-blackColor transition-colors duration-200 whitespace-nowrap ${
+                        isCollapsed ? "xl:hidden" : ""
+                      }`}
                     >
                       {item.label}
                     </span>
@@ -499,13 +510,15 @@ const Sidebar: React.FC<SidebarProps> = ({
                     <div className="flex gap-2 items-center">
                       <div className="w-[30px] h-[30px] group flex justify-center items-center flex-shrink-0 text-xl font-medium text-blackColor">
                         <item.icon
-                          className={`opacity-70 group-hover:opacity-100 transition-opacity duration-200 ${active ? "opacity-100" : ""
-                            }`}
+                          className={`opacity-70 group-hover:opacity-100 transition-opacity duration-200 ${
+                            active ? "opacity-100" : ""
+                          }`}
                         />
                       </div>
                       <span
-                        className={`text-base font-medium text-descriptionColor group-hover:text-blackColor transition-colors duration-200 whitespace-nowrap ${isCollapsed ? "xl:hidden" : ""
-                          }`}
+                        className={`text-base font-medium text-descriptionColor group-hover:text-blackColor transition-colors duration-200 whitespace-nowrap ${
+                          isCollapsed ? "xl:hidden" : ""
+                        }`}
                       >
                         {item.label}
                       </span>
@@ -532,8 +545,9 @@ const Sidebar: React.FC<SidebarProps> = ({
               <LogoutIcon />
             </div>
             <span
-              className={`text-base font-normal  whitespace-nowrap ${isCollapsed ? "xl:hidden" : ""
-                }`}
+              className={`text-base font-normal  whitespace-nowrap ${
+                isCollapsed ? "xl:hidden" : ""
+              }`}
             >
               Log Out Account
             </span>
