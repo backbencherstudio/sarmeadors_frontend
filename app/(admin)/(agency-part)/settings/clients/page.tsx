6ceb241c7752692c6303049518bbec49accea7e6
mahmudul-hasan01 <mahamudul2018@gmail.com>
2026-05-02 11:22:54 +0600

    Add Clients.

--- app/(admin)/(agency-part)/settings/clients/page.tsx
+++ app/(admin)/(agency-part)/settings/clients/page.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import AccessControlAndVisibilitySettings from "@/components/agency/globalSetting/candidates/AccessControlAndVisibilitySettings";
+import CandidateDashboardSettings from "@/components/agency/globalSetting/candidates/CandidateDashboardSettings";
+import CandidateTypesTagsChecklistSettings from "@/components/agency/globalSetting/candidates/CandidateTypesTagsChecklistSettings";
+import Documents from "@/components/agency/globalSetting/candidates/Documents";
+import ProfileSettings from "@/components/agency/globalSetting/candidates/ProfileSettings";
+import ProfileSettings2 from "@/components/agency/globalSetting/candidates/ProfileSettings2";
+import RegistrationFeeSettings from "@/components/agency/globalSetting/candidates/RegistrationFeeSettings";
+import ScheduleAvailabilitySettings from "@/components/agency/globalSetting/candidates/ScheduleAvailabilitySettings";
+import React from "react";
 
 export default function ClientsPage() {
-    return (
-        <div>ClientsPage</div>
-    )
+  return (
+    <div className="space-y-4">
+      <CandidateDashboardSettings />
+      <CandidateTypesTagsChecklistSettings />
+      <ProfileSettings />
+      <AccessControlAndVisibilitySettings />
+      <RegistrationFeeSettings />
+      <Documents />
+      <ProfileSettings2 />
+      <ScheduleAvailabilitySettings />
+    </div>
+  );
 }
