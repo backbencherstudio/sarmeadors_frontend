0e6ce5e7000a20642e45601fa78ff7a89ddfdb69
Utso Sarkar <utsosarkar7870@gmail.com>
2026-05-27 12:00:00 +0600

    Rating setting update4

--- components/dashboard/Form-Customization/MiddleFieldsColumn.tsx
+++ components/dashboard/Form-Customization/MiddleFieldsColumn.tsx
@@ -362,6 +362,7 @@ export default function MiddleFieldsColumn() {
           ))
         ) : (
           <div className="text-gray-400">
+            
             <div>
               <h2 className="text-lg md:text-xl lg:text-2xl font-semibold text-headerColor">
                 {activeBlock.name}
