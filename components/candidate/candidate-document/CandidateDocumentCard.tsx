75a7430408f305407cc09fc3f1ff4a9215f910f9
Utso Sarkar <utsosarkar7870@gmail.com>
2026-06-28 08:49:10 +0600

    Dev: Candidate document show done

--- components/candidate/candidate-document/CandidateDocumentCard.tsx
+++ components/candidate/candidate-document/CandidateDocumentCard.tsx
@@ -3,18 +3,8 @@ import ButtonReuseable from "@/components/reusable/CustomButton";
 import DeleteIcon from "@/public/icon/DeleteIcon";
 import documentBlackIcon from "@/public/icon/RequiredBalackIcon.png";
 import documentGreenIcon from "@/public/icon/requiredGreenIcon.png";
+import { RequiredDocument } from "@/types";
 import Image from "next/image";
-import CandidateDocumentCardSkeleton from "./CandidateDocumentCardSkeleton";
-
-export type CandidateDocumentItem = {
-  id: number;
-  title: string;
-  subtitle: string;
-  isUploaded: boolean;
-  previewUrl?: string;
-  previewName?: string;
-  previewType?: string;
-};
 
 function CandidateDocumentCard({
   item,
@@ -24,20 +14,18 @@ function CandidateDocumentCard({
   onDelete,
   onUpload,
 }: {
-  item: CandidateDocumentItem;
+  item: RequiredDocument;
   isLoading?: boolean;
-  onView?: (item: CandidateDocumentItem) => void;
-  onEdit?: (item: CandidateDocumentItem) => void;
-  onDelete?: (item: CandidateDocumentItem) => void;
-  onUpload?: (item: CandidateDocumentItem) => void;
+  onView?: (item: RequiredDocument) => void;
+  onEdit?: (item: RequiredDocument) => void;
+  onDelete?: (item: RequiredDocument) => void;
+  onUpload?: (item: RequiredDocument) => void;
 }) {
-  if (isLoading) return <CandidateDocumentCardSkeleton />;
-
   return (
     <div className="rounded-xl border border-borderColor hover:shadow-lg transition-shadow duration-200 bg-bgColor px-6 py-10">
       <div className="mx-auto flex w-full  flex-col items-center text-center">
         <Image
-          src={item.isUploaded ? documentGreenIcon : documentBlackIcon}
+          src={!item.can_upload ? documentGreenIcon : documentBlackIcon}
           alt="Document status"
           width={82}
           height={82}
@@ -47,16 +35,16 @@ function CandidateDocumentCard({
         <h3 className="mt-4 text-xl font-semibold text-headerColor">
           {item.title}
         </h3>
-        <p className="mt-1 text-base text-secondaryColor">{item.subtitle}</p>
+        <p className="mt-1 text-base text-secondaryColor">{item.description}</p>
 
-        {item.previewUrl && !item.previewType?.startsWith("image/") && (
+        {/* {item.previewUrl && !item.previewType?.startsWith("image/") && (
           <p className="mt-2 max-w-[240px] truncate text-sm text-secondaryColor">
             {item.previewName}
           </p>
-        )}
+        )} */}
 
         <div className="mt-5 flex items-center gap-2">
-          {item.isUploaded ? (
+          {!item.can_upload ? (
             <>
               <ButtonReuseable
                 type="button"
--- components/candidate/candidate-document/CandidateDocumentPage.tsx
+++ components/candidate/candidate-document/CandidateDocumentPage.tsx
@@ -1,70 +1,29 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import CandidateDocumentCard, {
-  type CandidateDocumentItem,
-} from "./CandidateDocumentCard";
-
-const candidateDocuments: CandidateDocumentItem[] = [
-  {
-    id: 1,
-    title: "Please upload a headshot of yourself",
-    subtitle: "You’ve already signed this agreement.",
-    isUploaded: false,
-  },
-  {
-    id: 2,
-    title: "Driver's license or government issued card",
-    subtitle: "You’ve already signed this agreement.",
-    isUploaded: true,
-  },
-  {
-    id: 3,
-    title: "Letter(s) of recommendation",
-    subtitle: "You’ve already signed this agreement.",
-    isUploaded: true,
-  },
-  {
-    id: 4,
-    title: "Additional Letter(s) of recommendation",
-    subtitle: "You’ve already signed this agreement.",
-    isUploaded: true,
-  },
-  {
-    id: 5,
-    title: "Additional Letter(s) of recommendation",
-    subtitle: "You’ve already signed this agreement.",
-    isUploaded: false,
-  },
-  {
-    id: 6,
-    title: "Nanny Resume",
-    subtitle:
-      "Make sure your resume highlights your nanny and other childcare experience",
-    isUploaded: true,
-  },
-];
+import { useGetCandidateDocumentQuery } from "@/feature/slice/candidate/candidate-dashboard/candidateDocumentSlice";
+import { RequiredDocument } from "@/types";
+import CandidateDocumentCard from "./CandidateDocumentCard";
+import CandidateDocumentCardSkeleton from "./CandidateDocumentCardSkeleton";
 
 function CandidateDocumentPage() {
-  const [isLoading, setIsLoading] = useState(true);
+  const { data: documents, isLoading } =
+    useGetCandidateDocumentQuery("Documents");
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsLoading(false);
-    }, 700);
-
-    return () => clearTimeout(timer);
-  }, []);
+  console.log(documents);
 
   return (
     <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-      {candidateDocuments.map((item) => (
-        <CandidateDocumentCard
-          key={item.id}
-          item={item}
-          isLoading={isLoading}
-        />
-      ))}
+      {isLoading
+        ? [...Array(4)].map((_, index) => (
+            <CandidateDocumentCardSkeleton key={index} />
+          ))
+        : documents?.data?.required_documents?.map((item: RequiredDocument) => (
+            <CandidateDocumentCard
+              key={item.key}
+              item={item}
+              isLoading={isLoading}
+            />
+          ))}
     </div>
   );
 }
