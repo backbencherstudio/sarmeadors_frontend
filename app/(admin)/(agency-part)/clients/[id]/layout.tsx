b29bd55d5cb519f0c18c9ede9988e827aaeb0bba
Utso Sarkar <utsosarkar7870@gmail.com>
2026-08-01 11:01:01 +0600

    Dev: agency clients page update

--- app/(admin)/(agency-part)/clients/[id]/layout.tsx
+++ app/(admin)/(agency-part)/clients/[id]/layout.tsx
@@ -8,7 +8,7 @@ export default async function ClientLayout({
   params,
 }: {
   children: React.ReactNode;
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }) {
   const { id } = await params;
 
--- components/clients/AdminTabs/AdminTabs.tsx
+++ components/clients/AdminTabs/AdminTabs.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import ReusableTabs from "@/components/reusable/ReusableTabs";
+import { useParams } from "next/navigation";
+import { use } from "react";
 import { FaTrophy } from "react-icons/fa";
 import {
   HiOutlineCreditCard,
@@ -12,40 +14,43 @@ import {
 import { MdSms } from "react-icons/md";
 
 export function AdminTabs() {
+  const params = useParams()
+  console.log(params);
+  
   const tabs = [
     {
       label: "Lists",
-      link: "/clients/admin/list",
+      link: `/clients/${params.id}/admin/list`,
       icon: <HiOutlineMenu className="w-5 h-5" />,
     },
     {
       label: "Notes",
-      link: "/clients/admin/notes",
+      link: `/clients/${params.id}/admin/notes`,
       icon: <HiOutlineDocumentText className="w-5 h-5" />,
     },
     {
       label: "Email and SMS",
-      link: "/clients/admin/email-sms",
+      link: `/clients/${params.id}/admin/email-sms`,
       icon: <MdSms className="w-5 h-5" />,
     },
     {
       label: "Events",
-      link: "/clients/admin/events",
+      link: `/clients/${params.id}/admin/events`,
       icon: <FaTrophy className="w-5 h-5" />,
     },
     {
       label: "Password",
-      link: "/clients/admin/password",
+      link: `/clients/${params.id}/admin/password`,
       icon: <HiOutlineLockClosed className="w-5 h-5" />,
     },
     {
       label: "Documents",
-      link: "/clients/admin/documents",
+      link: `/clients/${params.id}/admin/documents`,
       icon: <HiOutlineDocument className="w-5 h-5" />,
     },
     {
       label: "Payments",
-      link: "/clients/admin/payments",
+      link: `/clients/${params.id}/admin/payments`,
       icon: <HiOutlineCreditCard className="w-5 h-5" />,
     },
   ];
